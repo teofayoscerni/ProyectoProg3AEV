c47c4c5d6c4612dea4b541fc65e9d112db77510c
teofayoscerni <75489027+teofayoscerni@users.noreply.github.com>
2025-04-26 19:43:11 +0200

    Update ConexionBD.java

--- Teo_fayos_cerni_gestionTienda/src/models/ConexionBD.java
+++ Teo_fayos_cerni_gestionTienda/src/models/ConexionBD.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class ConexionBD {
     private static final String URL ="jdbc:mysql://localhost:3306/ejercicio_evaluable";
     private static final String USER ="root";
-    private static final String PASSWORD ="abril2005teo";
+    private static final String PASSWORD ="";
 
     public static Connection conect(){
         try {
